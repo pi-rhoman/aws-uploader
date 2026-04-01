c3fba3488b2e0391a616632b6c3675652f6b05f9
Liam Lenihan <llenihan21@gmail.com>
2026-04-01 12:05:12 +0100

    split and upload symultaneously

--- backup-maker.sh
+++ backup-maker.sh
@@ -2,34 +2,11 @@
 
 # Where do we store the fragments
 # by default store in current directory
-usage () {
-	printf '%s\n' "Usage: ./backup-maker -f <fragments directory> [backup file]" >&2 && exit 1
-}
-while getopts ":f:" flag
-do
-	case "${flag}" in
-		f) fragmentsdir=${OPTARG};; 
-		*) $(usage) || exit $?
-	esac
-done
-shift $((OPTIND - 1))
-
 BACKUP_FILE=`realpath $1`
 BACKUP_FILENAME=`basename $BACKUP_FILE`
 BUCKET_NAME=liams-computer-backup
 FRAGMENT_COUNT=200
-# Get the CRC32 of the file
-CRC=`crc32 $BACKUP_FILE`
 
-# Split the file into fragments
-if [ ! $fragmentsdir ]; then
-	$(usage) || exit $?
-elif [ ! -d $fragmentsdir ]; then
-	printf '%s\n' "fragments directory does not exist" >&2
-	exit 1
-fi
-cd $fragmentsdir
-split -n $FRAGMENT_COUNT --numeric-suffixes=1 $BACKUP_FILE $BACKUP_FILENAME. 
 
 get_upload () {
 	echo `aws s3api list-multipart-uploads --bucket $BUCKET_NAME | jq ".Uploads.[] | select(.Key == \"$BACKUP_FILENAME\")"`
@@ -47,11 +24,18 @@ else
 fi
 UPLOADED_PARTS=`aws s3api list-parts --bucket $BUCKET_NAME --key $BACKUP_FILENAME --upload-id $UPLOAD_ID`	
 
+FILE_PARTS="{
+	\"Parts\": []
+}" 
 # Upload each part individually
-for part in ./$BACKUP_FILENAME*; do
-	# get the part number from the file extension
-	PART_NUMBER=${part##*.}
-	# Find the previously uploaded version of the part if it extsts
+PART_NUMBER=1
+while [ $PART_NUMBER -lt $FRAGMENT_COUNT ]; do
+	
+
+	# extract the right chunk
+	split -n $PART_NUMBER/$FRAGMENT_COUNT --numeric-suffixes=1 $BACKUP_FILE > $BACKUP_FILE.$PART_NUMBER
+	part=$BACKUP_FILE.$PART_NUMBER
+	# check for prior uploads
 	PRIOR_UPLOAD=`echo $UPLOADED_PARTS | jq --argjson part_number $PART_NUMBER '.Parts.[] | select(.PartNumber == $part_number)'`
 	if [ ! -z "$PRIOR_UPLOAD" ] ; then
 		echo "Checking $PART_NUMBER"
@@ -60,24 +44,24 @@ for part in ./$BACKUP_FILENAME*; do
 		if [ "$CRC_MATCHES" == "false" ] ; then
 			echo "Checksum mismatch with uploaded part $PRIOR_UPLOAD expected $PART_CRC32. Reuploading..."
 		else
+			# remove the old part
+			rm $part
+			((PART_NUMBER++))
 			continue
+			
 		fi 
 	fi
-	echo "Uploading $PART_NUMBER"
-	aws s3api upload-part --bucket $BUCKET_NAME --key $BACKUP_FILENAME --part-number $PART_NUMBER --body $part --upload-id $UPLOAD_ID --checksum-algorithm CRC32
+	# save attributes for completing the upload
+	ETAG=($(md5sum $part))
+	FILE_PARTS=`echo $FILE_PARTS | jq --argjson part_number $PART_NUMBER --arg part_crc32 "$PART_CRC32" --arg etag "${ETAG}" '.Parts += [{"PartNumber":$part_number, ETag:$etag, ChecksumCRC32:$part_crc32}]'`
+ 	# upload the part
+	aws s3api upload-part --bucket $BUCKET_NAME --key $BACKUP_FILENAME --part-number $PART_NUMBER --body $BACKUP_FILE.$PART_NUMBER --upload-id $UPLOAD_ID --checksum-algorithm CRC32
+	# remove the old part
+	rm $part
+	((PART_NUMBER++))
 done
 
+
 # Complete the upload
-FILE_PARTS="{
-	\"Parts\": []
-}" 
-for part in ./$BACKUP_FILENAME*; do
-	PART_NUMBER=${part##*.}
-	
-	PART_CRC32=`crc32 $part | xxd -r -p | base64`
-	ETAG=($(md5sum $part))
-	FILTER='.Parts += [{"PartNumber":$part_number, ETag:$etag, ChecksumCRC32:$part_crc32}]'
-	FILE_PARTS=`echo $FILE_PARTS | jq --argjson part_number $PART_NUMBER --arg part_crc32 "$PART_CRC32" --arg etag "${ETAG}" $FILTER` 
-done
 echo $FILE_PARTS > fileparts.json
 aws s3api complete-multipart-upload --multipart-upload file://fileparts.json --bucket $BUCKET_NAME --key $BACKUP_FILENAME --upload-id $UPLOAD_ID --checksum-type FULL_OBJECT
